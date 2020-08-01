40a20cd86239e960b06c3bae22188352134bcbc3
ksllive <zhenya-kiselev-97@mail.ru>
2020-08-02 02:23:28 +0300

    Done Interface.

--- WeatherApp/Weather/WeatherCell.swift
+++ WeatherApp/Weather/WeatherCell.swift
@@ -9,9 +9,7 @@
 import UIKit
 
 class WeatherCell: UICollectionViewCell {
-    
     @IBOutlet weak var weather: UILabel!
     @IBOutlet weak var icon: UIImageView!
     @IBOutlet weak var time: UILabel!
-    
 }
--- WeatherApp/Weather/WeatherViewController.swift
+++ WeatherApp/Weather/WeatherViewController.swift
@@ -15,16 +15,6 @@ class WeatherViewController: UICollectionViewController {
 
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
     // MARK: UICollectionViewDataSource
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
@@ -33,13 +23,11 @@ class WeatherViewController: UICollectionViewController {
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-// Вернем 10 ячеек:
         return 10
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 
-// Из пула:
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "WeatherCell", for: indexPath) as! WeatherCell
         cell.weather.text = "30 C°"
         cell.time.text = "20.02.2020 20:02"
