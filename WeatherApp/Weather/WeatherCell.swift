537b7e488bb4629d1350bceac42b8e25540e0552
ksllive <zhenya-kiselev-97@mail.ru>
2020-08-31 11:08:18 +0300

    Cell for collection.

--- WeatherApp/Weather/WeatherCell.swift
+++ WeatherApp/Weather/WeatherCell.swift
@@ -10,7 +10,27 @@ import UIKit
 
 class WeatherCell: UICollectionViewCell {
     
+    @IBOutlet weak var shadowView: UIView! {
+        didSet {
+            self.shadowView.layer.shadowOffset = .zero
+            self.shadowView.layer.shadowOpacity = 0.75
+            self.shadowView.layer.shadowRadius = 6
+            self.shadowView.backgroundColor = .clear
+        }
+    }
+    @IBOutlet weak var containerView: UIView! {
+        didSet {
+            self.containerView.clipsToBounds = true
+        }
+    }
     @IBOutlet weak var weather: UILabel!
     @IBOutlet weak var icon: UIImageView!
     @IBOutlet weak var time: UILabel!
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        self.shadowView.layer.shadowPath = UIBezierPath(ovalIn: self.shadowView.bounds).cgPath
+        self.containerView.layer.cornerRadius = containerView.frame.size.width / 2
+    }
 }
